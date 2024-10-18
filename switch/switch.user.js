0afaf24060dad02cbcafdb1797f7439c2f1bfce0
sdnazarova <sd.nazarova@tensor.ru>
2024-10-18 16:57:40 +0300

    поставила новую версию

--- switch/switch.user.js
+++ switch/switch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         switch
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      2.0.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://dev-new-edu.sbis.ru/*
