0f940054d26d022e50074f46eaf5c07a57fa2c92
sdnazarova <sd.nazarova@tensor.ru>
2024-10-18 16:57:02 +0300

    сделала работу без лишнего перехода по странице

--- switch/switch.user.js
+++ switch/switch.user.js
@@ -15,38 +15,78 @@
 
 
 (({ document }) => {
+    const style = document.createElement('style')
+    style.type = 'text/css'
+    style.innerHTML = `
+    .SDO-switch-block {
+       position: fixed;
+       right: 0px;
+       top: 0px;
+       font-size: 14px;
+       background: rgb(228 190 245);
+       padding: 0px 5px 5px 10px;
+       border-radius: 0px 0px 0px 15px;
+       cursor: pointer;
+       z-index: 10000000000000000000000000000000000000;
+       display: flex;
+       flex-direction: column;
+    }
+    .SDO-switch-block .non-active {
+       display:none;
+    }
+  `
+    document.head.append(style);
     const TaperMonkeyGetCookie = (name) =>{
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
       ));
       return matches ? decodeURIComponent(matches[1]) : false;
 }
+    let closed_sdo_block = true;
 
-    let cookie_user = TaperMonkeyGetCookie("rootmoodle");
+    let cookie_type = "rootmoodle";
     if (location.host !== 'dev-new-edu.sbis.ru') {
-        cookie_user = TaperMonkeyGetCookie("rootsabyenglish");
+        cookie_type = "rootsabyenglish";
     }
+
+    let cookie_user = TaperMonkeyGetCookie(cookie_type);
     if (cookie_user == false) cookie_user = 1;
     if (cookie_user !== false) {
-        let switch_block = document.createElement("span");
+        let switch_block = document.createElement("div");
         let users = ["Мичурина", "Назарова", "Иванова", "Еремычева"];
-        switch_block.innerText = `Режим: ${users[cookie_user - 1]}`;
-        switch_block.style = `
-            position: fixed;
-            right: 0px;
-            top: 0px;
-            font-size: 14px;
-            background: rgb(228 190 245);
-            padding: 0px 5px 5px 10px;
-            border-radius: 0px 0px 0px 15px;
-            cursor: pointer;
-            z-index: 10000000000000000000000000000000000000;
-        `;
-        switch_block.addEventListener("click", (e)=>{
-            if (location.host == 'dev-new-edu.sbis.ru') location.href="/tensor/switch/"
-            else  location.href="/switch/"
-        })
+        switch_block.classList.add("SDO-switch-block");
+        users.forEach((element, index) => {
+            if (index == cookie_user - 1) {
+                switch_block.innerHTML += `<span class="active">Выбрано: ${element}</span>`;
+            } else {
+                switch_block.innerHTML += `<span class="non-active">${element}</span>`;
+            }
+        });
         document.body.insertAdjacentElement("afterbegin", switch_block)
+        let non_actives = document.querySelectorAll(".SDO-switch-block .non-active")
+        let spans = document.querySelectorAll(".SDO-switch-block span")
+        spans.forEach((element, index) => {
+            element.addEventListener("click", (e)=>{
+                let w = index + 1;
+                if (w != cookie_user) {
+                    document.cookie = `${cookie_type}=${w}; domain=.`+location.hostname+`; path=/; secure=true`
+                    location.reload();
+                }
+            })
+        })
+
+        switch_block.addEventListener("click", (e)=>{
+            if (closed_sdo_block) {
+                non_actives.forEach((element, index) => {
+                    element.style.display = "block";
+                })
+            } else {
+                non_actives.forEach((element, index) => {
+                    element.style.display = "";
+                })
+            }
+            closed_sdo_block = !closed_sdo_block
+        })
     }
 })(
     // @ts-ignore
